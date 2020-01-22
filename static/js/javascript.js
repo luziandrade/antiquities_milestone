8e055e8bf1bbe387289b818f8280719eb45f6aef
Luana Andrade <luzi015@hotmail.com>
2020-01-22 19:48:47 +0000

    accordion

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -6,21 +6,3 @@ $('.navTrigger').click(function () {
 
 });
 
-
-(function($) {
-	'use strict';
-
-	jQuery(document).on('ready', function(){
-
-			$('a.page-scroll').on('click', function(e){
-				var anchor = $(this);
-				$('html, body').stop().animate({
-					scrollTop: $(anchor.attr('href')).offset().top - 50
-				}, 1500);
-				e.preventDefault();
-			});
-
-	});
-
-
-})(jQuery);
