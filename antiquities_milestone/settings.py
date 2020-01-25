bd288728d2fc3acafbd3d52c01d9f5134057dab5
Luana Andrade <luzi015@hotmail.com>
2020-01-25 20:58:13 +0000

    heroku

--- antiquities_milestone/settings.py
+++ antiquities_milestone/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
