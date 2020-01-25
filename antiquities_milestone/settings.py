204f5bf0c355fa781ceb29f1e22233f2c162bbae
Luana Andrade <luzi015@hotmail.com>
2020-01-25 18:28:15 +0000

    heroku

--- antiquities_milestone/settings.py
+++ antiquities_milestone/settings.py
@@ -14,7 +14,6 @@ import os
 from django.contrib import messages
 #import env
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,13 +140,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # djang-heroku setting
-django_heroku.settings(locals())
 
 STATICFILES_LOCATION = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
