b4cca0535027ca7212dabb54a37232cdd3b5b967
Luana Andrade <luzi015@hotmail.com>
2020-01-25 18:07:49 +0000

    heroku

--- antiquities_milestone/settings.py
+++ antiquities_milestone/settings.py
@@ -14,6 +14,7 @@ import os
 from django.contrib import messages
 #import env
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,6 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# djang-heroku setting
+django_heroku.settings(locals())
 
 STATICFILES_LOCATION = 'static'
 STATIC_URL = '/static/'
--- antiquities_milestone/urls.py
+++ antiquities_milestone/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.static import serve
 from django.views import static
 from .settings import MEDIA_ROOT
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,5 +12,5 @@ urlpatterns = [
     path('search/', include('search.urls')),
     path('checkout/', include('checkout.urls')),
     path('bids/', include('bidding.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    re_path(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+]
