b6282ad198228bd378784574ecc46d1ef0bd7b12
Luana Andrade <luzi015@hotmail.com>
2020-01-25 19:10:52 +0000

    heroku

--- antiquities_milestone/settings.py
+++ antiquities_milestone/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'antiquities_milestone.urls'
@@ -140,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # djang-heroku setting
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_LOCATION = 'static'
 STATIC_URL = '/static/'
 
@@ -148,6 +149,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
