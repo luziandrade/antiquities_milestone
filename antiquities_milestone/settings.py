824d61ce9ccb32c562492dc675c951fcbf7685f2
Luana Andrade <luzi015@hotmail.com>
2020-01-26 01:05:09 +0000

    missing background image

--- antiquities_milestone/settings.py
+++ antiquities_milestone/settings.py
@@ -87,15 +87,6 @@ MESSAGE_TAGS = {
 }
 WSGI_APPLICATION = 'antiquities_milestone.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# }
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 else:
